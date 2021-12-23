a08a47ee1799915b1be76708582b2a6fbc250b9d
zipris <longvavet1325@gmail.com>
2021-12-24 00:45:47 +0700

    [FEAT] Create Producer Beer management page (CRUD)

---
+++ pages/dashboard/producers/_id/index.vue
@@ -0,0 +1,229 @@
+<template>
+  <div class="container-fluid">
+    <div class="breadcrumb-container">
+      <Breadcrumb :items="breadcrumbItems" />
+    </div>
+    <div class="add-beer-container">
+      <div class="left"><sidebar-admin /></div>
+      <div class="right">
+        <form id="needs-validation">
+          <div class="right__top">
+            <p class="notion required-note">(*) Bắt buộc</p>
+          </div>
+          <div class="form-group info__content__name">
+            <label for=""
+              >Tên nhà sản xuất <span class="required-note">*</span></label
+            >
+
+            <div class="input-box">
+              <input
+                id="name"
+                v-model="producers.name"
+                type="text"
+                class="form-control"
+                disabled
+                required
+              />
+              <div class="invalid-feedback">
+                Tên nhà sản xuất không được bỏ trống !
+              </div>
+            </div>
+          </div>
+
+          <div class="form-group info__content__address">
+            <label for=""
+              >Địa chỉ nhà sản xuất<span class="required-note">*</span></label
+            >
+
+            <div class="input-box">
+              <input
+                v-model="producers.address"
+                type="text"
+                class="form-control"
+                disabled
+                required
+              />
+              <div class="invalid-feedback">
+                Vui lòng địa chỉ nhà sản xuất !
+              </div>
+            </div>
+          </div>
+          <div v-if="!editting" class="action">
+            <button class="btn btn-danger" @click="removeProducers">Xóa</button>
+            <button class="btn btn-primary" @click="changeState">
+              Chỉnh sửa
+            </button>
+          </div>
+          <div v-else class="action">
+            <button class="btn btn-danger" @click="changeState">Hủy</button>
+            <button class="btn btn-primary" @click="updateProducers">
+              Lưu
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import Breadcrumb from '~/components/Breadcrumb.vue'
+import SidebarAdmin from '~/components/SidebarAdmin.vue'
+export default {
+  components: {
+    Breadcrumb,
+    SidebarAdmin,
+  },
+  computed: {
+    producerId() {
+      return this.$route.params.id
+    },
+  },
+  data() {
+    return {
+      breadcrumbItems: [
+        { name: 'Trang chủ', url: '/' },
+        { name: 'Quản lý', url: '/dashboard' },
+        { name: 'Nhà sản xuất', url: '/dashboard/producers' },
+        {
+          name: 'Chi tiết nhà sản xuẩt',
+          url: `/dashboard/producers/${this.producerId}`,
+        },
+      ],
+      producers: [],
+      editting: false,
+      PRODUCER_URL: '/beer/producer/',
+    }
+  },
+  async created() {
+    if (process.client) {
+      const authToken = localStorage.getItem('auth._token.google')
+      try {
+        const response = await axios.get(
+          `/api/v1${this.PRODUCER_URL}${this.producerId}`,
+          {
+            headers: { Authorization: authToken },
+          }
+        )
+        this.producers = response.data
+        console.log(this.producers)
+      } catch (err) {
+        alert(err)
+      }
+    }
+  },
+  methods: {
+    changeState(event) {
+      event.preventDefault()
+      this.editting = !this.editting
+      const inputs = document.getElementsByClassName('form-control')
+      const notes = document.getElementsByClassName('required-note')
+      for (let i = 0; i < inputs.length; i++) {
+        inputs[i].disabled = !inputs[i].disabled
+      }
+      for (let i = 0; i < notes.length; i++) {
+        notes[i].style.visibility =
+          notes[i].style.visibility === 'visible' ? 'hidden' : 'visible'
+      }
+    },
+
+    validate(event) {
+      const form = document.getElementById('needs-validation')
+      event.preventDefault()
+      form.classList.add('was-validated')
+
+      if (form.checkValidity() === false) {
+        event.stopPropagation()
+        return false
+      }
+      return true
+    },
+    async updateProducers(event) {
+      const isValid = this.validate(event)
+      if (isValid) {
+        if (process.client) {
+          const authToken = localStorage.getItem('auth._token.google')
+          try {
+            await axios.patch(
+              `/api/v1${this.PRODUCER_URL}${this.producerId}/`,
+              this.producers,
+              {
+                headers: { Authorization: authToken },
+              }
+            )
+            this.$router.push('/dashboard/producers')
+          } catch (err) {
+            alert(err)
+          }
+        }
+      }
+    },
+    async removeProducers(event) {
+      event.preventDefault()
+      if (process.client) {
+        const authToken = localStorage.getItem('auth._token.google')
+        try {
+          await axios.delete(`/api/v1${this.PRODUCER_URL}${this.producerId}/`, {
+            headers: { Authorization: authToken },
+          })
+          this.$router.push('/dashboard/producers')
+        } catch (err) {
+          alert(err)
+        }
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.add-beer-container {
+  display: flex;
+  flex-direction: row;
+  .left {
+    width: 300px;
+  }
+  .right {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    align-items: center;
+    margin-bottom: 40px;
+
+    &__top {
+      display: flex;
+      width: 100%;
+      justify-content: space-between;
+      margin-bottom: 20px;
+    }
+    .notion {
+      width: 100%;
+      text-align: left;
+    }
+    .required-note {
+      color: red;
+      visibility: hidden;
+    }
+  }
+}
+.form-control {
+  box-shadow: none;
+}
+.form-group {
+  display: flex;
+  flex-direction: row;
+
+  label {
+    width: 200px;
+  }
+  .form-select,
+  input,
+  textarea {
+    width: 400px;
+  }
+}
+.btn {
+  width: 100px;
+}
+</style>
---
+++ pages/dashboard/producers/index.vue
@@ -0,0 +1,161 @@
+<template>
+  <div class="container-fluid">
+    <div class="breadcrumb-container">
+      <Breadcrumb :items="breadcrumbItems" />
+    </div>
+    <div class="users-container">
+      <div class="left"><sidebar-admin /></div>
+      <div class="right">
+        <div class="total-users container-fluid">
+          <span>Tất cả nhà sản xuất: {{ producers.length }}</span>
+
+          <div class="search-bar">
+            <form class="form-inline">
+              <input
+                class="form-control mr-sm-2"
+                type="search"
+                placeholder="Tìm kiếm"
+                aria-label="Search"
+              />
+              <button
+                class="btn btn-outline-success my-2 my-sm-0"
+                type="submit"
+              >
+                <i class="fas fa-search"></i>
+              </button>
+            </form>
+            <div class="add-button">
+              <button
+                class="btn btn-outline-success my-2 my-sm-0 ml-2"
+                @click="showModal = true"
+              >
+                Thêm NXS
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="user-list container-fluid">
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">STT</th>
+                <th scope="col">Tên</th>
+                <th scope="col">Địa chỉ</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="(producer, index) in producers"
+                :key="producer.id"
+                class="user-list__item"
+                @click="$router.push(`producers/${producer.id}`)"
+              >
+                <th scope="row">{{ index + 1 }}</th>
+                <td>{{ producer.name }}</td>
+                <td>{{ producer.address }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="pagination">
+          <div class="arrow-box">
+            <span class="previos-arrow pagination__arrow"
+              ><i class="far fa-arrow-alt-circle-left"></i
+            ></span>
+            <span class="next-arrow pagination__arrow"
+              ><i class="far fa-arrow-alt-circle-right"></i
+            ></span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <Modal v-show="showModal" @close-modal="showModal = false" />
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import Breadcrumb from '~/components/Breadcrumb.vue'
+import SidebarAdmin from '~/components/SidebarAdmin.vue'
+import Modal from '~/components/Modal/ProducerModal.vue'
+export default {
+  components: { Breadcrumb, SidebarAdmin, Modal },
+  middleware: 'auth',
+  data() {
+    return {
+      showModal: false,
+      breadcrumbItems: [
+        { name: 'Trang chủ', url: '/' },
+        { name: 'Quản lý', url: '/dashboard' },
+        { name: 'Nhà sản xuất', url: '/dashboard/producers' },
+      ],
+      producers: [],
+    }
+  },
+  async created() {
+    const URL = '/beer/producer/';
+
+    if (process.client){
+      const authToken = localStorage.getItem("auth._token.google");
+      const response = await axios.get(`/api/v1${URL}`, {
+        headers: {"Authorization" : authToken}
+      });
+      this.producers = response.data;
+    }
+    
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~assets/scss/variables';
+
+.users-container {
+  display: flex;
+  flex-direction: row;
+  .left {
+    width: 300px;
+  }
+  .right {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  }
+}
+.total-users {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+}
+.search-bar {
+  margin-top: 10px;
+  margin-bottom: 10px;
+  display: flex;
+  .form-control {
+    box-shadow: none;
+  }
+}
+.user-list__item {
+  cursor: pointer;
+  &:hover {
+    background-color: $hoverSidebar;
+  }
+}
+.pagination {
+  display: flex;
+  justify-content: center;
+  .arrow-box {
+    width: 100px;
+    display: flex;
+    justify-content: space-between;
+  }
+  &__arrow {
+    font-size: 30px;
+  }
+}
+.add-button {
+  button {
+  }
+}
+</style>
