47b935aabc2d72194be504c26a7710e545208ed6
ductai26998 <ductai26998@gmail.com>
2022-02-02 08:28:07 +0700

    add chart

---
+++ components/Chart/LineChart.vue
@@ -0,0 +1,40 @@
+
+
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  props: ['data'],
+  data() {
+    return {
+      options: {
+        responsive: true,
+        fill: false,
+        legend: {
+          display: false,
+        },
+        title: {
+          display: true,
+          text: 'Monthly Income',
+        },
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      },
+    }
+  },
+  mounted() {
+    this.renderChart(this.data, this.options)
+  },
+}
+</script>
+
+<style>
+</style>
--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,13 +1,54 @@
 <template>
-  <div></div>
+  <div class="container-fluid">
+    <div class="breadcrumb-container">
+      <Breadcrumb />
+    </div>
+    <div class="users-container">
+      <LineChart :data="lineChartData" :height="200" />
+    </div>
+  </div>
 </template>
 <script>
-import { roleGuard } from '~/helper/helper'
+import LineChart from '~/components/Chart/LineChart.vue'
+import Breadcrumb from '~/components/Breadcrumb.vue'
 
 export default {
-  middleware: ['auth', roleGuard('ADMIN')],
-  mounted() {
-    this.$router.push('/dashboard/beers')
+  components: { Breadcrumb, LineChart },
+  layout: 'admin',
+  data() {
+    return {
+      duration: 'month',
+      type: 'sale',
+      lineChartData: {
+        labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        datasets: [
+          {
+            label: 'My First Dataset',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1,
+          },
+        ],
+      },
+    }
+  },
+  async created() {
+    const URL = '/beer/chart_data/'
+    if (process.client) {
+      const authToken = localStorage.getItem('auth._token.local')
+      try {
+        const response = await this.$axios.get(
+          `/api/v1${URL}?duration=${this.duration}&type=${this.type}`,
+          {
+            headers: { Authorization: authToken },
+          }
+        )
+        console.log(response.data)
+      } catch (err) {
+        alert(err)
+      }
+    }
   },
 }
 </script>
